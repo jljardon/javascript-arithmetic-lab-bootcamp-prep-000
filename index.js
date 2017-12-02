464b07cb6b39e2c7c518c53bf2958a8a75127df7
Jose Luis Jardon <jljardon.c@gmail.com>
2017-12-02 06:37:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 function add(a,b){
-  var result = a+b
+  var result = a + b
   return result
 }
 
 function substract(a,b){
-  var result = a-b
+  var result = a - b
   return result
 }
 
 function multiply(a,b){
-  var result = a*b
+  var result = a * b
+  return result
+}
+
+function divide(a,b){
+  var result = a / b 
   return result
 }
